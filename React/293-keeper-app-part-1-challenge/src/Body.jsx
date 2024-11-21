87d101a5b44164b702e0630e6aa544de66fa3763
Kiran Pradeep <kiranpradeep499@gmail.com>
2024-11-21 23:11:28 +0530

    Committing Keeper app part one

---
+++ React/293-keeper-app-part-1-challenge/src/Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Body(){
+    return (
+        <body className="note">
+            <h1>This is the note title</h1>
+            <p>This is the note content</p>
+        </body>
+    )
+}
+
+export default Body;
